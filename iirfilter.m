c849b6dc3d266eb77f590b28f646d923c566cc72
Christian Porsmose <chrbp4@gmail.com>
2024-05-11 11:56:53 +0200

    matlab

--- iirfilter.m
+++ iirfilter.m
@@ -11,17 +11,15 @@ fs = 2000;
 freqz(b,a,[],fs)
 
 
-
-
 %%
-% Define the name for the C files
+%script for putting num and denum in a c header
 numeratorFilename = 'numerator_coeffs.h';
+denominatorFilename = 'denominator_coeffs.h';
 
 
 % Open the first C file for writing
 fid = fopen(numeratorFilename, 'w');
 
-% Write the numerator coefficients to the first C file
 fprintf(fid, 'const double numerator_coeffs[%d][2] = {\n', size(sos, 1));
 for i = 1:size(sos, 1)
     fprintf(fid, '    {%.15f, %.15f, %.15f}', sos(i,1) , sos(i, 2), sos(i, 3));
@@ -33,20 +31,10 @@ for i = 1:size(sos, 1)
 end
 fprintf(fid, '};\n');
 
-% Close the first C file
 fclose(fid);
 
-disp(['Numerator coefficients exported to ', numeratorFilename]);
-
-
-
-% Define the name for the second C file
-denominatorFilename = 'denominator_coeffs.h';
-
-% Open the second C file for writing
 fid = fopen(denominatorFilename, 'w');
 
-% Write the denominator coefficients to the second C file
 fprintf(fid, 'const double denominator_coeffs[%d][3] = {\n', size(sos, 1));
 for i = 1:size(sos, 1)
     fprintf(fid, '    {%.15f, %.15f}', sos(i, 5), sos(i, 6));
@@ -58,7 +46,6 @@ for i = 1:size(sos, 1)
 end
 fprintf(fid, '};\n');
 
-% Close the second C file
+
 fclose(fid);
 
-disp(['Denominator coefficients exported to ', denominatorFilename]);
